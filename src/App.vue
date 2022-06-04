8c3d1308869c9eeaacfa349e97fa315c07d4e53c
aibayanyu <aibayanyu@qq.com>
2022-06-04 09:11:56 +0800

    feat: add dark demo

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+// 获取当前系统的主题色
+const colorScheme = usePreferredColorScheme()
+console.log(colorScheme.value)
+// 获取当前存储的状态
+</script>
+
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
     <router-view />
